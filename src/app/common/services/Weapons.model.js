f34e46a3defd26c678650009d9c34e1c8f67dfbf
Ryan DeStefano <rdestefa@nd.edu>
2020-07-07 22:42:21 -0400

    Initial Weapons Model

---
+++ src/app/common/services/Weapons.model.js
@@ -0,0 +1,47 @@
+/* Model and helped methods for the Weapons parse object */
+
+class WeaponsModel {
+    constructor(Parse) {
+        this.Parse = Parse;
+        this.data = {};
+        this.collection = [];
+        this.name = 'Weapons';
+        this.fields = [
+            'name',
+            'health',
+            'healthRegen',
+            'armor',
+            'magicResist',
+            'attackDamage',
+            'attackSpeed'
+        ];
+    }
+    
+    New(obj) {
+        if (angular.isUndefined(obj)) {
+            const parseObject = new this.Parse.Object(this.name);
+            this.Parse.defineAttributes(parseObject, this.fields);
+            return ParseObject;
+        } else {
+            this.Parse.defineAttributes(obj, this.fields);
+            return obj;
+        }
+    }
+    
+    getById(id) {
+        return new this.Parse.Query(this.New())
+            .get(id)
+            .then(result => {
+                this.Parse.defineAttributes(result, this.fields);
+                this.data = result;
+                return Promise.resolve(result);
+            })
+            .catch(error => Promise.reject(error));
+    }
+    
+    //getAllWeapons()
+}
+
+angular
+    .module('common')
+    .service('WeaponsModel', WeaponsModel);
