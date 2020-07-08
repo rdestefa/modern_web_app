dbdca4dd6d78626292781d2b968fa740c05083ce
Ryan DeStefano <rdestefa@nd.edu>
2020-07-08 16:10:03 -0400

    Initial Commit

--- src/app/common/app.run.js
+++ src/app/common/app.run.js
@@ -1,4 +1,21 @@
 function init($rootScope, $uiRouter, $mdSidenav, $timeout, $log) {
     var Visualizer = window['ui-router-visualizer'].Visualizer;
     var pluginInstance = $uiRouter.plugin(Visualizer);
+    
+    $rootScope.isEmpty = isEmpty;
+
+// Utility function for checking if JS object is empty
+    function isEmpty(obj) {
+        for (var key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                return false;
+            }
+        }
+    
+        return true;
+    }
 }
+
+angular
+    .module('common')
+    .run(init)
